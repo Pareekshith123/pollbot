1fe50020f523d97d119d9c89fdccbf008f9e3fdd
Pareekshith TG <pareekshithtg@gmail.com>
2023-04-10 16:02:54 +0530

    chatbot-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpSupportComponent } from './help-support/help-support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpSupportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
